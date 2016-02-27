94e5b10a9066109ecdeab27a9d64b5ea8f19d668
Fernando Duran <duran.fernando@gmail.com>
2016-02-26 20:31:59 -0500

    Create command.py

---
+++ command.py
@@ -0,0 +1,11 @@
+import shlex, subprocess
+
+
+def execute(command):
+    ''' Executes exernal command '''
+    # shlex.split tokenizes command into a list
+    args = shlex.split(command)
+    res = subprocess.Popen(args, stdout = subprocess.PIPE)
+    print res.stdout.read().strip()
+
+execute('ls -l')
