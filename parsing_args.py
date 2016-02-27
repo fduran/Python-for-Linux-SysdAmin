2c211b1772c97df930a61c5a406594ce7fdff7db
Fernando Duran <duran.fernando@gmail.com>
2016-02-26 21:24:46 -0500

    Parsing arguments, command line options

---
+++ parsing_args.py
@@ -0,0 +1,17 @@
+# optparse deprecated in 2.7
+import optparse
+
+
+p = optparse.OptionParser()
+p.add_option("-i", "--inputfile", action="store", type="string", dest="infile")
+opt, args = p.parse_args()
+
+print "in file: ", opt.infile
+
+# use argparse instead in 2.7 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", action="store", dest="infile") # type string is default
+args = parser.parse_args()
+print "in file: ", args.infile
