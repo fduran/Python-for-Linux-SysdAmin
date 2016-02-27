0fe859ac877cf2531126d9115937e516511863e7
Fernando Duran <duran.fernando@gmail.com>
2016-02-26 20:39:35 -0500

    Implementation of 'grep' using 're'

---
+++ grep_regexp.py
@@ -0,0 +1,17 @@
+import re
+
+def grep(afile, asearch):
+    ''' find asearch in afile '''
+    reobj = re.compile(asearch)
+    matches = 0
+    with open(afile) as fd:
+        for line in fd.readlines():
+            if reobj.search(line):
+                matches += 1
+                print line.strip()
+    
+    print '----'
+    print "%d matches found" % matches
+
+
+grep("scores.txt","john")
